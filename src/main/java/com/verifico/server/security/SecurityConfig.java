82b63c521644d20ebd65ea0f210bcc7c93997dd1
Ashutosh Kuinkel <108271619+hejjwheh@users.noreply.github.com>
2026-01-03 16:11:30 +1100

    fix: bypass security for auth endpoints

--- src/main/java/com/verifico/server/security/SecurityConfig.java
+++ src/main/java/com/verifico/server/security/SecurityConfig.java
@@ -2,13 +2,25 @@ package com.verifico.server.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
-  
+
   @Bean
-  public BCryptPasswordEncoder passwordEncoder(){
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http
+        .authorizeHttpRequests(
+            (requests) -> requests.requestMatchers(HttpMethod.POST,"/api/auth/register", "/api/auth/login").permitAll()
+                .anyRequest().authenticated());
+    return http.build();
+  }
+
+  @Bean
+  public BCryptPasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
   }
 }
