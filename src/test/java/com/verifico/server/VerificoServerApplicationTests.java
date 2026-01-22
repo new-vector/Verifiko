c8d7ba0e65a68cda9456294fd98aacada56acbf4
Ashutosh Kuinkel <108271619+hejjwheh@users.noreply.github.com>
2026-01-22 18:56:23 +1100

    ⭐ REMOVE SPRINGBOOTTEST ANNOTATION SO APP DOESN'T LOAD ON UNIT TESTS

--- src/test/java/com/verifico/server/VerificoServerApplicationTests.java
+++ src/test/java/com/verifico/server/VerificoServerApplicationTests.java
@@ -1,9 +1,9 @@
 package com.verifico.server;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest removing because it causes the whole app to load when doing unit tests
 // implement test containers for CI so we don't need to spin up db manually.
 class VerificoServerApplicationTests {
 
