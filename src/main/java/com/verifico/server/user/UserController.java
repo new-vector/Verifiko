3911b77a7ba5f998014891092014bb500c1a6332
Ashutosh Kuinkel <108271619+hejjwheh@users.noreply.github.com>
2026-02-01 17:44:49 +1100

    fix: Remove unused import

--- src/main/java/com/verifico/server/user/UserController.java
+++ src/main/java/com/verifico/server/user/UserController.java
@@ -1,6 +1,5 @@
 package com.verifico.server.user;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
