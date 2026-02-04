59a8c476286b89f030898d8e486af24e08ea4b4b
Ashutosh Kuinkel <108271619+hejjwheh@users.noreply.github.com>
2026-02-04 22:00:15 +1100

    feat: CreditTransactionRepository setup

--- src/main/java/com/verifico/server/credit/CreditTransactionRepository.java
+++ src/main/java/com/verifico/server/credit/CreditTransactionRepository.java
@@ -1,5 +1,9 @@
 package com.verifico.server.credit;
 
-public class CreditTransactionRepository {
-  
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CreditTransactionRepository extends JpaRepository<CreditTransaction, Long> {
+  Page<CreditTransaction> findByUserIdOrderByCreatedAtDesc(Long userId, Pageable pageable);
 }
