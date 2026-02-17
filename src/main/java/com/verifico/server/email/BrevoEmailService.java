103a1af4fe647f553e1fdb1ac9b9ddb852bebb14
Ashutosh Kuinkel <108271619+hejjwheh@users.noreply.github.com>
2026-02-17 22:55:33 +1100

    feat: initialise email service files

--- src/main/java/com/verifico/server/email/BrevoEmailService.java
+++ src/main/java/com/verifico/server/email/BrevoEmailService.java
@@ -1,4 +1,5 @@
 package com.verifico.server.email;
 
 public class BrevoEmailService {
+
 }
--- src/main/java/com/verifico/server/email/EmailService.java
+++ src/main/java/com/verifico/server/email/EmailService.java
@@ -1,4 +1,6 @@
 package com.verifico.server.email;
 
 public interface EmailService {
+//    call our individual send email functions here
+//    e.g void sendPasswordChangedNotification(User user);
 }
