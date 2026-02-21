80b8ff31f83de2a26eb9648b6483a2fcfbc84cc4
Ashutosh Kuinkel <108271619+hejjwheh@users.noreply.github.com>
2026-02-22 10:09:27 +1100

    feat: forgot+reset password endpoint service initialised

--- src/main/java/com/verifico/server/auth/PasswordResetService.java
+++ src/main/java/com/verifico/server/auth/PasswordResetService.java
@@ -1,5 +1,23 @@
 package com.verifico.server.auth;
 
+import java.security.SecureRandom;
+import java.time.LocalDateTime;
+import java.util.Base64;
+import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.verifico.server.auth.dto.ResetPasswordRequest;
+import com.verifico.server.email.EmailService;
+import com.verifico.server.user.User;
+import com.verifico.server.user.UserRepository;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+
 // overall flow outline:
 // user clicks forgot pass in frontend
 // user enters email... if email found
@@ -10,3 +28,79 @@ package com.verifico.server.auth;
 // then prompt them to change password
 // reference article:
 // https://medium.com/@AlexanderObregon/making-a-basic-password-reset-workflow-in-spring-boot-b845bb4f7cf9
+
+@Service
+@RequiredArgsConstructor
+public class PasswordResetService {
+
+  private final UserRepository userRepository;
+  private final ResetTokenRepository resetTokenRepository;
+  private final PasswordEncoder passwordEncoder;
+  private final EmailService emailService;
+  private static final SecureRandom secureRandom = new SecureRandom();
+  private static final Base64.Encoder base64Encoder = Base64.getUrlEncoder().withoutPadding();
+
+  public void passResetRequest(String email) {
+    Optional<User> userOpt = userRepository.findByEmail(email.strip().toLowerCase());
+
+    if (userOpt.isEmpty())
+      return;
+
+    User user = userOpt.get();
+
+    // cleanup prior tokens, avoids case of having 2 available reset links for one
+    // user in the case
+    // that one expired yet and user gets another link:
+    resetTokenRepository.deleteById(user.getId());
+
+    String token = makeResetToken();
+
+    ResetToken resetToken = new ResetToken();
+    resetToken.setToken(token);
+    resetToken.setUser(user);
+    // Let users know in the email the link will expire after 15 mins..
+    resetToken.setExpiresAt(LocalDateTime.now().plusMinutes(15));
+
+    resetTokenRepository.save(resetToken);
+
+    // send email to the given user with the token included... here:
+    // key point: Nothing leaves server unless token is valid & tied to
+    // an existing user:
+    emailService.changePasswordResetLinkEmailForv1(token, email);
+  }
+
+  public void validateToken(String token) {
+    Optional<ResetToken> tokenOpt = resetTokenRepository.findByToken(token);
+
+    if (tokenOpt.isEmpty() || tokenOpt.get().isExpired()) {
+      throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid or expired token");
+    }
+  }
+
+  @Transactional
+  public void resetPassword(ResetPasswordRequest request) {
+    Optional<ResetToken> tokenOpt = resetTokenRepository.findByToken(request.getToken());
+
+    if (tokenOpt.isEmpty() || tokenOpt.get().isExpired()) {
+      throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Token is invalid or expired");
+    }
+
+    ResetToken fetchedToken = tokenOpt.get();
+    User user = fetchedToken.getUser();
+
+    user.setPassword(passwordEncoder.encode(request.getNewPassword()));
+    userRepository.save(user);
+    resetTokenRepository.delete(fetchedToken);
+
+    // notifying user of password change:
+    emailService.sendPasswordChangedEmailForv1(user);
+  }
+
+  // helpers:
+  public String makeResetToken() {
+    byte[] randomBytes = new byte[32]; // 256 bit token
+    secureRandom.nextBytes(randomBytes);
+    return base64Encoder.encodeToString(randomBytes);
+  }
+
+}
